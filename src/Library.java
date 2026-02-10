b9b3db917055ef48f4dede6306a53a5aa191d8f1
aiperi <kydykbekovaajperi@gmail.com>
2026-02-10 16:55:08 +0600

    Implement book management methods in Library class
    Implemented methods to add, display, borrow, and return books in the library.

--- src/Library.java
+++ src/Library.java
@@ -10,26 +10,54 @@ public class Library {
 
     // TODO: Add book to array
     public void addBook(Book book) {
+        if (count < books.length) {
+            books[count++] = book;
+        } else {
+            System.out.println("There is no place");
+        }
         // implement
     }
 
     // TODO: Display all books
     public void displayBooks() {
-        // implement
+        for (int i = 0; i < count; i++) {
+            System.out.println(books[i].toString());
+        }
     }
 
     // TODO: Search book by title
     public Book searchByTitle(String title) {
+        for (int i = 0; i < count; i++) {
+            if (books[i].getTitle() == title) {
+                return books[i];
+            }
+        }
         return null;
     }
 
+
     // TODO: Borrow book by title
     public void borrowBook(String title) {
-        // implement
+        for (int y = 0; y < count; y++) {
+            if (books[y].getTitle().equalsIgnoreCase(title) && books[y].isAvailable()) {
+                books[y].borrowBook();
+                System.out.println("you borrowed :" +books[y].getTitle());
+            }
+            else {
+                System.out.println("Book is not available");
+            }
+        }
     }
 
     // TODO: Return book by title
     public void returnBook(String title) {
-        // implement
+        Book u = searchByTitle(title);
+        if (u != null){
+            u.returnBook();
+            System.out.println("You returned book : "+u.getTitle());
+        }
+        else {
+            System.out.println("book not found in library's archive");
+        }
     }
 }
